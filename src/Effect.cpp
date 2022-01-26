bad6d27aea04e31cdc6d5f5a42c901156adb797c
Igor Grkavac <grkavac.igor@gmail.com>
2022-01-26 16:07:06 +0100

    Add helper function to get effect type from char

--- src/Effect.cpp
+++ src/Effect.cpp
@@ -1,1 +1,11 @@
 #include "Effect.h"
+
+Effect::EffectType Effect::GetEffectType(const char &str) {
+
+  if (str == 'l') {
+    return EffectType::Backlight;
+  } else if (str == 'b')
+    return EffectType::Breathing;
+
+  return EffectType::Backlight;
+}
--- src/Effect.h
+++ src/Effect.h
@@ -5,9 +5,11 @@ class Effect {
 public:
   enum EffectType { Backlight, Breathing };
 
+  static EffectType GetEffectType(const char &str);
+
   Effect() {}
 
-  Effect(EffectType type, uint8_t _intensity, uint8_t _breathDuration)
+  Effect(EffectType type, uint8_t _intensity, uint8_t _breathDuration = 1)
       : effect{type}, intensity{_intensity}, breathDuration{_breathDuration} {}
 
   EffectType effect;
